5466b3f670e8e4571107330c907a9a819b22ea30
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-07-26 13:40:41 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"*",
+        origin:"https://insta-clone-m.vercel.app/",
         methods:["GET","POST"]
     }
 })
