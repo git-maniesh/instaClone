68ee551ccb02f1423502c3328e8750ae311e234e
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-07-26 13:40:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"*",
+    origin:"https://insta-clone-m.vercel.app/",
     credentials:true
 }))
 app.use(express.json())
