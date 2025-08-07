5c7b63261400f40ffc7e747eb5b29c2e3a575285
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-08-07 20:55:17 +0530

    Update VideoPlayer.jsx

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -41,7 +41,7 @@ useEffect(()=>{
     }, []) 
   return (
     <div className='h-[100%] relative cursor-pointer max-w-full rounded-2xl overflow-hidden'>
-      <video ref={videoTag} src={media} autoPlay loop muted={mute} className='h-[100%] cursor-pointer w-full object-cover rounded-2xl' onClick={handleClick}/>
+      <video ref={videoTag} src={media} autoPlay loop muted className='h-[100%] cursor-pointer w-full object-cover rounded-2xl' onClick={handleClick}/>
   <div className='absolute bottom-[10px] right-[10px]' onClick={()=>setMute(prev=>!prev)}>
     {!mute?<FiVolume2 className='w-[20px] h-[20px] text-white font-semibold'/>:<FiVolumeX className='w-[20px] h-[20px] text-white font-semibold'/>}
   </div>
