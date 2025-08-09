95c7c8937a9be0d61cdccbe869099236d814e0c3
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-08-09 11:08:08 +0530

    Update VideoPlayer.jsx

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -6,7 +6,7 @@ import { FiVolume2 } from "react-icons/fi";
 import { FiVolumeX } from "react-icons/fi";
 function VideoPlayer({media}) {
     const videoTag=useRef()
-    const [mute,setMute]=useState(false)
+    const [mute,setMute]=useState(true)
     const [isPlaying,setIsplaying]=useState(true)
 const handleClick=()=>{
 if(isPlaying){
@@ -41,7 +41,7 @@ useEffect(()=>{
     }, []) 
   return (
     <div className='h-[100%] relative cursor-pointer max-w-full rounded-2xl overflow-hidden'>
-      <video ref={videoTag} src={media} autoPlay loop muted className='h-[100%] cursor-pointer w-full object-cover rounded-2xl' onClick={handleClick}/>
+      <video ref={videoTag} src={media} autoPlay loop muted={mute} className='h-[100%] cursor-pointer w-full object-cover rounded-2xl' onClick={handleClick}/>
   <div className='absolute bottom-[10px] right-[10px]' onClick={()=>setMute(prev=>!prev)}>
     {!mute?<FiVolume2 className='w-[20px] h-[20px] text-white font-semibold'/>:<FiVolumeX className='w-[20px] h-[20px] text-white font-semibold'/>}
   </div>
